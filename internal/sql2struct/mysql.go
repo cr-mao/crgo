794629449a26a26465ffcb04893bad5db6f56ad6
maozhongyu <mao.zhongyu@qianka.com>
2021-03-15 23:19:09 +0800

    sql2struct

---
+++ internal/sql2struct/mysql.go
@@ -0,0 +1,102 @@
+package sql2struct
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DBModel struct {
+	DBEngine *sql.DB
+	DBInfo   *DBInfo
+}
+
+//数据库信息
+type DBInfo struct {
+	DBType   string
+	Host     string
+	UserName string
+	Password string
+	Charset  string
+}
+
+// 字段属性
+type TableColumn struct {
+	ColumnName    string
+	DataType      string
+	IsNullable    string
+	ColumnKey     string
+	ColumnType    string
+	ColumnComment string
+}
+
+var DBTypeToStructType = map[string]string{
+	"int":        "int32",
+	"tinyint":    "int8",
+	"smallint":   "int32",
+	"mediumint":  "int32",
+	"bigint":     "int64",
+	"bit":        "int32",
+	"bool":       "bool",
+	"enum":       "string",
+	"set":        "string",
+	"varchar":    "string",
+	"char":       "string",
+	"tinytext":   "string",
+	"mediumtext": "string",
+	"text":       "string",
+	"longtext":   "string",
+	"blob":       "string",
+	"tinyblob":   "string",
+	"mediumblob": "string",
+	"longblob":   "string",
+	"date":       "time.Time",
+	"datetime":   "time.Time",
+	"timestamp":  "time.Time",
+	"time":       "time.Time",
+	"float":      "float64",
+	"double":     "float64",
+}
+
+func NewDBModel(info *DBInfo) *DBModel {
+	return &DBModel{DBInfo: info}
+}
+
+func (m *DBModel) Connect() error {
+	var err error
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s)/information_schema?charset=%s&parseTime=True&loc=Local",
+		m.DBInfo.UserName,
+		m.DBInfo.Password,
+		m.DBInfo.Host,
+		m.DBInfo.Charset,
+	)
+	m.DBEngine, err = sql.Open(m.DBInfo.DBType, dsn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *DBModel) GetColumns(dbName, tableName string) ([]*TableColumn, error) {
+	query := `
+		SELECT COLUMN_NAME,DATA_TYPE,COLUMN_KEY,IS_NULLABLE,COLUMN_TYPE,COLUMN_COMMENT FROM COLUMNS WHERE TABLE_SCHEMA = ?
+		AND TABLE_NAME = ?
+	`
+	rows, err := m.DBEngine.Query(query, dbName, tableName)
+	if err != nil {
+		return nil, err
+	}
+	if rows == nil {
+		return nil, errors.New("没有数据")
+	}
+	defer rows.Close()
+	var columns []*TableColumn
+	for rows.Next() {
+		var column TableColumn
+		rows.Scan(&column.ColumnName, &column.DataType, &column.ColumnKey, &column.IsNullable, &column.ColumnType, &column.ColumnComment)
+		columns = append(columns, &column)
+	}
+	return columns, nil
+}
