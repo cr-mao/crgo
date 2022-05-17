7dae731302f2fe40e65b7e4914af507324127ed9
毛忠宇 <595106153@qq.com>
2022-05-17 15:01:00 +0800

    error 处理

--- module/error/01warperror.go
+++ module/error/01warperror.go
@@ -2,27 +2,29 @@ package main
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/pkg/errors"
 )
 
 // 衍生goroutine
-func Go(x func()) {
-	go func() {
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Println(err)
-			}
-		}()
-		x()
-	}()
-}
+//func Go(x func()) {
+//	go func() {
+//		defer func() {
+//			if err := recover(); err != nil {
+//				fmt.Println(err)
+//			}
+//		}()
+//		x()
+//	}()
+//}
 
 func ReadFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
+		// 调用栈信息
 		return nil, errors.Wrap(err, "open failed")
 	}
 	defer f.Close()
@@ -33,20 +35,22 @@ func ReadFile(path string) ([]byte, error) {
 	return buf, nil
 }
 
+func ReadConfig() ([]byte, error) {
+	home := os.Getenv("HOME")
+	config, err := ReadFile(filepath.Join(home, ".setting.xml"))
 
-func ReadConfig()([]byte,error) {
-	home :=os.Getenv("HOME")
-	config,err :=ReadFile(filepath.Join(home,".setting.xml"))
-	return config,errors.WithMessage(err,"could not read config")
+	// errors.WithMessage error就追加message
+	return config, errors.WithMessage(err, "could not read config")
 }
 
-func main(){
-	_,err :=ReadConfig()
-	if err !=nil {
-		fmt.Printf("original error:%T %v\n",errors.Cause(err),errors.Cause(err))
-		fmt.Printf("stack stace :\n %+v\n",err)
+func main() {
+	_, err := ReadConfig()
+	if err != nil {
+		//根原因
+		fmt.Printf("original error:%T %v\n", errors.Cause(err), errors.Cause(err))
+
+		//
+		fmt.Printf("stack stace :\n %+v\n", err)
 		os.Exit(1)
 	}
 }
-
-
--- module/error/05errorshizhan.go
+++ module/error/05errorshizhan.go
@@ -1,7 +1,30 @@
 package main
 
-// todo
+import (
+	"errors"
+	"fmt"
+	perrors "github.com/pkg/errors"
+)
+
 // error 工程实战
 
+var my = errors.New("my")
 
+func main() {
+	err := test02()
+	fmt.Printf("%+v", err)
 
+}
+
+func test02() error {
+	return test01()
+}
+
+func test01() error {
+	return test0()
+}
+
+func test0() error {
+	return perrors.Wrapf(my, "test02 failed")
+
+}
