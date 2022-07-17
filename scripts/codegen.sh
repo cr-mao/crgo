ae5e09b03b4ff8809cb67954ef607f41e1815a5f
毛忠宇 <595106153@qq.com>
2022-07-17 11:08:43 +0800

    grpc 目录调整

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -1,2 +1,2 @@
-find grpcproject/biz -type f -name "*.pb.go" -delete
-protoc --proto_path=grpcproject/proto --go_out=plugins=grpc:grpcproject ./grpcproject/proto/helloworld.proto
+find grpc/biz -type f -name "*.pb.go" -delete
+protoc --proto_path=grpc/proto --go_out=plugins=grpc:grpc ./grpc/proto/helloworld.proto
